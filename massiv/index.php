83e1fcf7e1260d944b49e60a0e8cacb60a15a3a0
Artur <2004misiryun@gmail.com>
2023-01-16 15:21:13 +0300

    слайдер

--- massiv/index.php
+++ massiv/index.php
@@ -1,3 +1,16 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="slick/slick.css">
+    <link rel="stylesheet" type="text/css" href="slick/slick-theme.css">
+	<title>Document</title>
+</head>
+<body>
+	
+
 <table>
 		<tr>
 			<th>Номер</th>
@@ -11,13 +24,13 @@
  	  	"name" => "Artur",
  		"status" => "admin",
  		"years" => "19",
- 		"link" => "https://www.mvideo.ru/?utm_source=advcake&utm_medium=cpa&utm_campaign=62ec5fde&utm_content=affiliate&advcake_params=41c1f73344482bbf1b938e79b125f080&utm_term=41c1f73344482bbf1b938e79b125f080&ab_test_value=D1",
+ 		"link" => "https://www.citilink.ru",
  	],
  	[
  		"name" => "Dima",
  		"status" => "student",
  		"years"  => "18",
- 		"link" => "https://www.mvideo.ru/?utm_source=advcake&utm_medium=cpa&utm_campaign=62ec5fde&utm_content=affiliate&advcake_params=41c1f73344482bbf1b938e79b125f080&utm_term=41c1f73344482bbf1b938e79b125f080&ab_test_value=D1",
+ 		"link" => "https://www.citilink.ru",
  	],
  ];
  foreach($ISP as $number => $students){
@@ -25,7 +38,7 @@
 
  <tr>
  	<td><i><?php echo $number ?></i></td>
- 	<td class="name"><a href="<?php echo $value["link"] ?>"><?php echo $students["name"] ?></a></td>
+ 	<td class="name"><a href="<?php echo $students["link"] ?>"><?php echo $students["name"] ?></a></td>
  	<td class="status"><?php echo $students["status"] ?></td>
  	<td><?php echo $students["years"] ?></td>
  </tr>
@@ -33,6 +46,84 @@
 ?>
 
 </table>
+<div class="dicrim">
+	
+<?php
+ $a=1;
+ $b=11;
+ $c=1;
+ $d=($b * $b - 4 * $a * $c);
+ if ($d == 0) {
+ 	$x = ($b / 2 * $a);
+ 	echo "Корень равен: " .$x;
+ }elseif ($d > 0) {
+ 	$x1= (-$b + sqrt($d)) / 2 * $a . "<br>" ;
+ 	$x2= (-$b - sqrt($d)) / 2 * $a;
+ 	echo "Корень х1 равен: " .$x1;
+ 	echo "Корень x2 равен: " .$x2;
+
+ }else{
+ 	echo "Нет корней";
+ };
+
+ ?>
+</div>
+<div class="schoolboy">
+	<table>
+		<tr>
+			<th>Статус</th>
+			<th>Имя</th>
+			<th>Возраст</th>
+		</tr>
+	<?php
+	$group = [
+		[
+			"name" => "Artur",
+			"years" => "19",
+			"active" => true,
+			"link" => "https://www.citilink.ru"
+		],
+		[
+			"name" => "Dima",
+			"years" => "18",
+			"active" => false,
+			"link" => "https://www.citilink.ru"
+
+		],
+	];
+	foreach ($group as $key => $value) {
+	 ?>
+	 <?php if ($value["active"]){
+	 	$std_status = "Молодец";
+	 	$std_color = "green"; ?>
+	 <?php } else{ 
+	 	$std_status = "Прогульщик";
+	 	$std_color = "red";  ?>
+	<?php } ?>
+	<tr>
+	 	<td class="<?php echo $std_color ?>" ><?php echo $std_status?></td>
+	 	<td><a href="<?php echo $value["link"] ?>" target="_blank"><?php echo $value["name"]?></a></td>
+	 	<td><?php echo $value["years"] ?></td>
+	</tr>
+
+	  <?php 
+	  } ?>
+	</table>
+</div>
+
+<?php 
+$merin = array('picture' => "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRtok3DYZAMgRFYI1-SULOnQj1Eeo8f6hfWElPPmjEpWA&s" , );
+ foreach ($merin as $key => $value) {
+ ?>
+
+  <div class="multiple-items">
+    <div class="content"><img src="<?php echo $value["picture"] ?>"> your content</div>
+  </div>
+
+
+<?php } ?>
+
+
 <style>
 	table{
 		border: 1px solid red;
@@ -51,5 +142,36 @@
 		text-decoration: none;
 		color: purple;
 	}
+	.dicrim{
+	}
+	.green{
+		color: green;
+	}
+	.red{
+		color: red;
+	}
+	.content{
+		width: 250px;
+		height: 250px;
+	}
+</style>
 
-</style
+    <script type="text/javascript" src="jquery-1.11.0.min.js"></script>
+    <script type="text/javascript" src="jquery-migrate-1.2.1.min.js"></script>
+    <script type="text/javascript" src="slick/slick.min.js"></script>
+    <script type="text/javascript">
+        $(document).ready(function() {
+            $('.single-item').slick({
+
+            });
+        });
+        $(document).ready(function() {
+            $('.multiple-items').slick({
+                infinite: true,
+                slidesToShow: 1,
+                slidesToScroll: 1,
+            });
+        });
+    </script>
+</body>
+</html>
