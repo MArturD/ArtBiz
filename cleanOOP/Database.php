1bd264b2df5868b6d7aa8b23389796b00733b192
Artur <2004misiryun@gmail.com>
2023-02-27 03:54:49 +0300

    get and delete

--- cleanOOP/Database.php
+++ cleanOOP/Database.php
@@ -16,9 +16,16 @@ class Database {
         }
         return self::$instance;
 }
-    public function query($sql){
+    public function query($sql, $params = []){
         $this->error = false;
         $this->query = $this->pdo->prepare($sql);
+        if (count($params)){
+            $i = 1;
+            foreach ($params as $param){
+                $this->query->bindValue($i, $param);
+                $i++;
+            }
+        }
         if (!$this->query->execute()){
             $this->error = true;
         }else {
@@ -35,5 +42,33 @@ class Database {
     }
     public function result(){
         return $this->result;
+    }
+
+    public function get($table, $where = []){
+        return $this->action('SELECT *',$table,$where);
+    }
+
+    public function delete($table, $where = []){
+        return $this->action('DELETE',$table,$where);
+    }
+
+    public function action($action,$table,$where = []){
+        if (count($where) === 3){
+            $operators = ['=','<','>','<=','>='];
+
+            $field = $where[0];
+            $operator = $where[1];
+            $value = $where[2];
+
+            if (in_array($operator,$operators)){
+                $sql = "{$action} FROM {$table} WHERE {$field} {$operator} ?";
+//                var_dump($sql);die;
+                if (!$this->query($sql, [$value])->error()){
+                    return $this;
+                }
+
+            }
+        }
+        return false;
     }
 }
--- cleanOOP/index.php
+++ cleanOOP/index.php
@@ -1,6 +1,9 @@
 <?php
 include_once "Database.php";
-$users = Database::getInstance()->query("SELECT * FROM users");
+//$users = Database::getInstance()->query("SELECT * FROM users WHERE user_name IN (? , ?)", ['Artur','Dima']);
+$users = Database::getInstance()->get('users', ['user_name', '=', "Artur"]);
+//$users = Database::getInstance()->delete('users', ['user_name', '=', "Dima"]);
+
 //var_dump($users);
 
 
@@ -8,6 +11,6 @@ if ($users->error()){
     echo "This error";
 }else{
     foreach ($users->result() as $user){
-        echo $user['id'] . ['user_name'] . "<br>";
+        echo $user['id'] . $user['user_name'] . "<br>";
     }
 }
